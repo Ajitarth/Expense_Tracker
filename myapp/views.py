2998655bcc2795c281ac8ecfc59b9c0f0dfd56ad
User <user@Users-MacBook-Pro.local>
2024-02-05 16:14:17 +0530

    Added graphs and other expenses summary

--- myapp/views.py
+++ myapp/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import redirect, render
 from . forms import ExpenseForm
 from . models import Expense
+from django.db.models import Sum
+import datetime
 
 # Create your views here.
 
@@ -12,8 +14,25 @@ def index(request):
             expense.save()
             return redirect('index')
     expenses = Expense.objects.all()
+    total_expenses = expenses.aggregate(Sum('amount'))
+
+    last_year = datetime.date.today() - datetime.timedelta(days=365)
+    data = Expense.objects.filter(date__gt=last_year)
+    yearly_sum = data.aggregate(Sum('amount'))
+
+    last_month = datetime.date.today() - datetime.timedelta(days=30)
+    data = Expense.objects.filter(date__gt=last_month)
+    monthly_sum = data.aggregate(Sum('amount'))
+
+    last_week = datetime.date.today() - datetime.timedelta(days=7)
+    data = Expense.objects.filter(date__gt=last_week)
+    weekly_sum = data.aggregate(Sum('amount'))
+
+    daily_sums = Expense.objects.filter().values('date').order_by('date').annotate(sum=Sum('amount'))
+    categorical_sums = Expense.objects.filter().values('category').order_by('category').annotate(sum=Sum('amount'))
+
     expense_form = ExpenseForm()
-    return render(request, 'myapp/index.html', {'expense_form': expense_form, 'expenses': expenses})
+    return render(request, 'myapp/index.html', {'expense_form': expense_form, 'expenses': expenses,'total_expenses': total_expenses,'yearly_sum': yearly_sum, 'monthly_sum': monthly_sum,'weekly_sum': weekly_sum,'daily_sums': daily_sums,'categorical_sums':categorical_sums})
 
 
 
